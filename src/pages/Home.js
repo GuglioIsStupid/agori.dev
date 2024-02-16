ca5fd3bec7c010d3d1a973685bd01154212e98af
GuglioIsStupid <bradsbluetab@gmail.com>
2024-02-16 17:49:09 -0500

    Update projects list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,30 @@ const Projects = [
         screenshots: [],
         tags: ["rpg", "action", "game"]
     },
-    // TODO: Add more projects
+    {
+        name: "Not Too Bad",
+        description: "A comic series that is the canonical sequel to Guys Vs. Gods, continuing the story a few generations later. You'll follow Kurtis Guy, Charlie Guy, Tori Sargoth and Alice Reeds in an adventure to end a cycle started so long ago.",
+        link: "/projects/not-too-bad",
+        image: null,
+        screenshots: [],
+        tags: ["comic"]
+    },
+    {
+        name: "Challenges of Zandor",
+        description: "An isekai novel series that we're trying to put our spin on, follow Night Terrasurge and Samuel Rosé into their journey into an unknown world with many mysteries and other wonders. This is mainly focused on the worldbuilding and characters.",
+        link: "/projects/challenges-of-zandor",
+        image: null,
+        screenshots: [],
+        tags: ["novel", "isekai"]
+    },
+    {
+        name: "Shadow of Hazel",
+        description: "A novel within the mythical world of Terralore. Dragons have been attacking many areas of the island of Zetalor during a civil war, and only one can save the island, and inevitably the entire world.",
+        link: "/projects/shadow-of-hazel",
+        image: null,
+        screenshots: [],
+        tags: ["novel"]
+    },
 ]
 
 const Home = () => {
